27757c8f3514aec1c91495f11cb7a767edfba0b4
maja <maya-kut@mail.ru>
2025-05-03 20:10:19 +0300

    add DataTypeOverflow

--- src/main/java/DataTypeOverflow.java
+++ src/main/java/DataTypeOverflow.java
@@ -2,16 +2,16 @@ public class DataTypeOverflow {
     public static void main(String[] args) {
         float maxFloat = Float.MAX_VALUE;
         float minFloat = Float.MIN_VALUE;
-        float overFlowedVolume1 = maxFloat*2;
+        float overFlowedVolumeFloat = maxFloat*2;
         System.out.println("Максимальное значение float: " + maxFloat);
         System.out.println("Минимальное значение float: " + minFloat);
-        System.out.println("Переполнение float: " + overFlowedVolume1);
+        System.out.println("Переполнение float: " + overFlowedVolumeFloat);
 
         double maxDouble = Double.MAX_VALUE;
         double minDouble = Double.MIN_VALUE;
-        double overFlowedVolume2 = maxDouble*2;
+        double overFlowedVolumeDouble = maxDouble*2;
         System.out.println("Максимальное значение double: " + maxDouble);
         System.out.println("Минимальное значение double: " + minDouble);
-        System.out.println("Переполнение double: " + overFlowedVolume2);
+        System.out.println("Переполнение double: " + overFlowedVolumeDouble);
     }
 }
